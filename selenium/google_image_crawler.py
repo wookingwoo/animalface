99a4b30889d6da4ee1000fc9973f1478ba118336
JeongHeon Woo <contact@wookingwoo.com>
2020-10-11 14:50:35 +0900

    Delete melsound module and add in main program

--- selenium/google_image_crawler.py
+++ selenium/google_image_crawler.py
@@ -5,17 +5,27 @@ import urllib.request
 import os
 import datetime
 import winsound
-from melsound import music_domisol
-
-for melody, duration in music_domisol:
-    winsound.Beep(so1[melody], 1000 // duration)
-
-
-
-
-
 
 start_time = time.time()
+
+# 멜로디 모듈
+so1 = {'do': 261, 're': 293, 'mi': 329, 'pa': 349, 'sol': 391, 'ra': 440, 'si': 493, 'do5': 523, 'D5': 587}
+
+mel = ['do', 'mi', 'sol', ]
+dur = [3, 3, 3, ]
+
+mel2 = ['sol', ]
+dur2 = [2]
+
+mel3 = ['D5', 'D5', 'D5']
+dur3 = [4, 4, 4]
+
+music_domisol = zip(mel, dur)
+music_sol = zip(mel2, dur2)
+music_re5 = zip(mel3, dur3)
+
+# 멜로디 모듈 끝
+
 
 people_list = ["강아지상 여자 연예인",
                "고양이상 여자 연예인",
@@ -102,8 +112,8 @@ for people_index in range(len(people_list)):
             for melody, duration in music_sol:
                 winsound.Beep(so1[melody], 1000 // duration)
 
-            # if count ==51:
-            #     break
+            if count ==6:
+                break
         except:
             for melody, duration in music_re5:
                 winsound.Beep(so1[melody], 1000 // duration)
